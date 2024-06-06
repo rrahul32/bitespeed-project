bad8247d4ebb9aff9720f4bb1a518522f88de4fe
rahul-evolvier <rahul@evolvier.com>
2024-06-06 23:22:06 +0530

    database setup

---
+++ src/env.d.ts
@@ -0,0 +1,9 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    MYSQL_HOST: string;
+    MYSQL_PORT: string;
+    MYSQL_DATABASE: string;
+    MYSQL_USERNAME: string;
+    MYSQL_PASSWORD: string;
+  }
+}
